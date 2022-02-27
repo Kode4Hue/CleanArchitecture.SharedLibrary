068caa3e28d212bf5f55b865de7a45c4b6cb5f56
kodehue <hubertgraham@outlook.com>
2022-02-26 23:21:38 -0500

    added auth behaviours

---
+++ CleanArchitecture.SharedLibrary/Security/Behaviours/SimpleAuthorizationBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using CleanArchitecture.SharedLibrary.Account.Services;
+using CleanArchitecture.SharedLibrary.Security.Attributes;
+using CleanArchitecture.SharedLibrary.Security.Exceptions;
+using MediatR;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.SharedLibrary.Account.Behaviours
+{
+    public class SimpleAuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly ISimpleIdentityService _identityService;
+
+        public SimpleAuthorizationBehaviour(
+            ICurrentUserService currentUserService,
+            ISimpleIdentityService identityService)
+        {
+            _currentUserService = currentUserService;
+            _identityService = identityService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var authorizeAttributes = request.GetType().GetCustomAttributes<SimpleAuthorizeAttribute>();
+
+            if (authorizeAttributes.Any())
+            {
+                // Must be authenticated user
+                if (_currentUserService.UserId == null)
+                {
+                    throw new UnauthenticatedAccessException();
+                }
+
+                // Role-based authorization
+                var authorizeAttributesWithRoles = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Roles));
+
+                if (authorizeAttributesWithRoles.Any())
+                {
+                    var authorized = false;
+
+                    foreach (var roles in authorizeAttributesWithRoles.Select(a => a.Roles.Split(',')))
+                    {
+                        foreach (var role in roles)
+                        {
+                            var isInRole = await _identityService.IsInRoleAsync(_currentUserService.UserId, role.Trim());
+                            if (isInRole)
+                            {
+                                authorized = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    // Must be a member of at least one role in roles
+                    if (!authorized)
+                    {
+                        throw new ForbiddenAccessException();
+                    }
+                }
+
+                // Policy-based authorization
+                var authorizeAttributesWithPolicies = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Policy));
+                if (authorizeAttributesWithPolicies.Any())
+                {
+                    foreach (var policy in authorizeAttributesWithPolicies.Select(a => a.Policy))
+                    {
+                        var authorized = await _identityService.AuthorizeAsync(_currentUserService.UserId, policy);
+
+                        if (!authorized)
+                        {
+                            throw new ForbiddenAccessException();
+                        }
+                    }
+                }
+            }
+
+            // User is authorized / authorization not required
+            return await next();
+        }
+    }
+}
