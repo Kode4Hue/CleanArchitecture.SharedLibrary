3ebfa0ce6b71d959f5839d8eac1d3e8c8a9ac1d2
kodehue <hubertgraham@outlook.com>
2022-02-26 23:21:21 -0500

    added logging behaviours

---
+++ CleanArchitecture.SharedLibrary/Common/Behaviours/Logging/SimpleLoggingBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.SharedLibrary.Common.Behaviours.Logging
+{
+    public class SimpleLoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+    {
+
+        private readonly ILogger _logger;
+
+        public SimpleLoggingBehaviour(ILogger logger)
+        {
+            _logger  = logger;
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("Request: {Name} with Request Info: {@Request}",
+                requestName, request);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CleanArchitecture.SharedLibrary/Common/Behaviours/Logging/UserRequestLoggingBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.SharedLibrary.Account.Services;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.SharedLibrary.Common.Behaviours.Logging
+{
+    public class UserRequestLoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+    {
+        private readonly ILogger _logger;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly ISimpleIdentityService _identityService;
+
+        public UserRequestLoggingBehaviour(ILogger logger, 
+            ICurrentUserService currentUserService, ISimpleIdentityService identityService) 
+        {
+            _logger = logger;
+            _currentUserService = currentUserService;
+            _identityService = identityService;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUserService.UserId ?? string.Empty;
+            string userName = string.Empty;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                userName = await _identityService.GetUserNameAsync(userId);
+            }
+
+            _logger.LogInformation("Request: {Name} made by user with UserId: {@UserId} & Username: {@UserName}. Request Info: {@Request}",
+                requestName, userId, userName, request);
+        }
+    }
+}
