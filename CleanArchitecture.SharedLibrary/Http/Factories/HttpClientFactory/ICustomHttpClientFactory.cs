c08e3902de24aa8baaaa0b173082da67e6d32f78
kodehue <hubertgraham@outlook.com>
2022-08-08 19:55:38 -0500

    added container extension to register custom http client factory implementation and bumped package version to 1.1.7

--- CleanArchitecture.SharedLibrary/Http/Factories/HttpClientFactory/ICustomHttpClientFactory.cs
+++ CleanArchitecture.SharedLibrary/Http/Factories/HttpClientFactory/ICustomHttpClientFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 
-namespace CleanArchitecture.SharedLibrary.Http.Factories
+namespace CleanArchitecture.SharedLibrary.Http.Factories.HttpClientFactory
 {
     public interface ICustomHttpClientFactory
     {
