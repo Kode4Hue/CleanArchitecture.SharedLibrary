bcd3c1798eb381565a6f15a6d0c45cc85e51bb84
kodehue <hubertgraham@outlook.com>
2023-02-28 14:57:30 -0500

    - changed location of messaging service interface - changed parameter name from 'topic' to 'messageKey'

---
+++ CleanArchitecture.SharedLibrary/Messaging/Services/IMessagingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.SharedLibrary.Messaging.Services
+{
+    public interface IMessagingService<T> where T : class
+    {
+        Task PublishMessage(string messageKey, T messageContent, CancellationToken cancellationToken);
+    }
+}
