102917408018ee55d4f80c4b7f9e589dab56806e
kodehue <hubertgraham@outlook.com>
2023-02-28 16:27:04 -0500

    added an 'unknown' status for publisher message status for IMessagingService

--- CleanArchitecture.SharedLibrary/Messaging/Constants/PublisherMessageStatuses.cs
+++ CleanArchitecture.SharedLibrary/Messaging/Constants/PublisherMessageStatuses.cs
@@ -5,5 +5,6 @@
         public const string Published = "published";
         public const string PossiblyPublished = "possibly-published";
         public const string NotPublished = "not-published";
+        public const string Unknown = "unknown";
     }
 }
